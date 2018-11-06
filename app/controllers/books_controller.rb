17b8c7480be5d9f8c7b829ae390c5074ff6f9ab7
Ayush Verma <ayush.k1998@gmail.com>
2018-11-06 20:12:15 +0530

    application complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
   def index
+  	@search_term = params[:search_term] || 'rails'
+  	@books = Book.about(@search_term)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+	include HTTParty
+
+	base_uri 'https://www.googleapis.com/books/v1/volumes'
+
+	format :json
+
+	def self.about search_term
+		get("", query: {q: "intitle:#{search_term}"})["items"] || []
+	end
 end
