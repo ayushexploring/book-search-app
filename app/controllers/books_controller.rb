5fdb6c7312dcc9ab507c3ea7ec58d75f65aa3a4f
Ayush Verma <ayush.k1998@gmail.com>
2018-11-06 20:48:53 +0530

    returning topsellers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-  	@search_term = params[:search_term] || 'rails'
+  	@search_term = params[:search_term]
   	@books = Book.about(@search_term)
   end
 end
