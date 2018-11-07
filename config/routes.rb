332820e9857c090f427ef81f1d8be0c175c303ab
Ayush Verma <ayush.k1998@gmail.com>
2018-11-07 17:50:37 +0530

    finish header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'books/index'
   root 'books#index'
+
+  get 'books/bookdesc'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
